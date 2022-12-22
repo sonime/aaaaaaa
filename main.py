765405b00c205ab51b32a9cb93ffe6e691f22334
sonime <sonimefan@gmail.com>
2022-12-21 18:14:16 -0800

    commented shit

--- main.py
+++ main.py
@@ -4,26 +4,26 @@ import random
 root = Tk()
 root.geometry("800x600")
 
-diceSizeEntry = Entry(root, width = 20)
+diceSizeEntry = Entry(root, width = 20)#declares the entry for die size
 diceSizeEntry.pack()
 
-def RollDice():
-    diceSize = int(diceSizeEntry.get())
-    rollResult = str(random.randint(1,diceSize))
-    historyList.append(rollResult)
-    historyListVar.set(historyList)
+def RollDice():#function for actually rolling the dice
+    diceSize = int(diceSizeEntry.get())#gets the value from entry form
+    rollResult = str(random.randint(1,diceSize))#rolls the die and converts to string
+    historyList.append(rollResult)#adds the result to the list
+    historyListVar.set(historyList)#updates the history box
 
-rollDice = Button(root, text = "Roll the dice!", command = lambda: RollDice())
+rollDice = Button(root, text = "Roll the dice!", command = lambda: RollDice())#click to call the dice roller function
 rollDice.pack()
 
-clearHistory = Button(root, text = "Clear the history!",)
+clearHistory = Button(root, text = "Clear the history!",)#FIX THIS, NOT WORKING
 clearHistory.pack()
 
 historyLabel = Label(root, text = "History:").pack()
 
-historyList = []
-historyListVar = StringVar(value=historyList)
-rollHistory = Listbox(root, listvariable = historyListVar).pack()
+historyList = []#declares history list, empty
+historyListVar = StringVar(value = historyList)#declares the variable that actually goes in the box
+rollHistory = Listbox(root, listvariable = historyListVar).pack()#creates the history box
 
-root.mainloop()
+root.mainloop()#DO NOT REMOVE
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
