edcb5a7dd39bb792922d85e7aaf8bad88d0c9b6d
sonime <sonimefan@gmail.com>
2022-12-21 16:44:09 -0800

    pain

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ from tkinter import *
 import random
 
 root = Tk()
+
+
+selectedDiceSize = IntVar()
+diceSizeMenu = OptionMenu(root, selectedDiceSize, 1, 2, 3, 4, 5, 6)
+diceSizeMenu.pack()
+
+global diceSize
+diceSize = selectedDiceSize.get()
 def RollDice():
-    global rollResult
-    rollResult = random.randint(1,6)
+    rollResult = random.randint(1,diceSize)
     resultText = Label(root, text = rollResult)
     resultText.pack()
 
-poop = Button(root, text = "poop", command = RollDice)
+poop = Button(root, text = "poop", command = lambda: RollDice())
 poop.pack()
 
 root.mainloop()
