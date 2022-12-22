e8ad28ca9a1686dd2ca66811d2bf72a9eed9675b
sonime <sonimefan@gmail.com>
2022-12-21 18:24:35 -0800

    fixed clearing history, merging to master

--- main.py
+++ main.py
@@ -13,17 +13,22 @@ def RollDice():#function for actually rolling the dice
     historyList.append(rollResult)#adds the result to the list
     historyListVar.set(historyList)#updates the history box
 
+def clearHistory():#function for clearing history
+    historyList.clear()#wipes the list clean
+    historyListVar.set(historyList)#updates the list to be clear
+
 rollDice = Button(root, text = "Roll the dice!", command = lambda: RollDice())#click to call the dice roller function
 rollDice.pack()
 
-clearHistory = Button(root, text = "Clear the history!",)#FIX THIS, NOT WORKING
+clearHistory = Button(root, text = "Clear the history!", command = clearHistory)#clears the history
 clearHistory.pack()
 
 historyLabel = Label(root, text = "History:").pack()
 
+
 historyList = []#declares history list, empty
 historyListVar = StringVar(value = historyList)#declares the variable that actually goes in the box
-rollHistory = Listbox(root, listvariable = historyListVar).pack()#creates the history box
+rollHistory = Listbox(root, yscrollcommand = true, listvariable = historyListVar).pack()#creates the history box
 
 root.mainloop()#DO NOT REMOVE
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
